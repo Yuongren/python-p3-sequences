58da1aa74f601363d72f1870f774bd6841aeec7d
gitYoungren <youngrengitonga@gmail.com>
2024-12-02 07:39:26 +0300

    completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return 
+    fib_sequence = [0]
+    if length > 1:
+        fib_sequence.append(1)
+    for i in range(2, length):
+            next_fib = fib_sequence[i -1] + fib_sequence[i - 2]
+            fib_sequence.append(next_fib)
+    print(fib_sequence)
